832da3df52a824b54cb042a7414d27f93f74fece
Ben Smerd <bsmerd@adam.com.au>
2022-10-26 22:09:36 +1030

    added in link for projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 // TODO ADD IN A THREEJS ANIMATED CUBE TO THE RIGHT OR SOMETHING ANIMATED TO LOOK COOL
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="flex flex-col gap-3 font-mono w-3/6 h-5/6 p-20 ml-40">
@@ -11,9 +12,12 @@ const Home = () => {
         I like to build dynamic and creative front-end applications while
         integrating logical and smooth back-end servers and APIs.
       </h3>
-      <button className="w-60 mt-5 text-cyan-100 border-solid border-2 border-green-400 p-2 rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100">
+      <Link
+        className="w-60 mt-5 text-cyan-100 border-solid border-2 border-green-400 p-3 text-center rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100"
+        to="projects"
+      >
         Check out my Projects!
-      </button>
+      </Link>
     </div>
   );
 };
