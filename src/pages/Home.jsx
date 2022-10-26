74fae6781bd9b5c52beaf0e395694289ec9fe303
Ben Smerd <bsmerd@adam.com.au>
2022-10-26 22:04:53 +1030

    homepage content created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
+// TODO ADD IN A THREEJS ANIMATED CUBE TO THE RIGHT OR SOMETHING ANIMATED TO LOOK COOL
 const Home = () => {
   return (
-    <div className="">
-      <small>Hi, my name is</small>
-      <h1>Ben Smerd</h1>
-      <h3>
+    <div className="flex flex-col gap-3 font-mono w-3/6 h-5/6 p-20 ml-40">
+      <small className="text-green-400">Hi, my name is</small>
+      <h1 className="text-8xl text-zinc-400 font-bold">Ben Smerd.</h1>
+      <h2 className="text-zinc-50 text-5xl">
+        I am a full-stack web developer.
+      </h2>
+      <h3 className="text-zinc-400">
         I like to build dynamic and creative front-end applications while
         integrating logical and smooth back-end servers and APIs.
       </h3>
-      <button>Check out my Projects!</button>
+      <button className="w-60 mt-5 text-cyan-100 border-solid border-2 border-green-400 p-2 rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100">
+        Check out my Projects!
+      </button>
     </div>
   );
 };
