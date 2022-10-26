775252537cf8ffacc1dbbb46139344a606bcfae5
Ben Smerd <bsmerd@adam.com.au>
2022-10-26 21:45:09 +1030

    made navigation links work with location and pathname and added resume download button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="flex justify-between h-20 p-10 font-mono">
       <div className="flex gap-5 items-center">
         <img src={close} alt="me" className="w-12 h-12 rounded-3xl" />
-        <h1 className="text-green-400">Smerdy</h1>
+        <h1 className="text-green-400 text-lg">Smerdy</h1>
       </div>
       <Navigation />
     </header>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,55 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import React from "react";
 
 const Navigation = () => {
+  const location = useLocation();
+  // const active = location.pathname.split("/")[1] === path.replace("/", "");
   return (
     <nav className="flex items-center">
-      <ul className="flex">
-        <Link className="text-green-400" to="/">
+      <ul className="flex gap-16 items-center">
+        <Link
+          className={
+            location.pathname === "/"
+              ? "text-green-400 text-lg"
+              : "text-cyan-100"
+          }
+          to="/"
+        >
           Home
         </Link>
-        <Link className="text-cyan-100" to="about">
+        <Link
+          className={
+            location.pathname === "/about"
+              ? "text-green-400 text-lg"
+              : "text-cyan-100"
+          }
+          to="about"
+        >
           About
         </Link>
-        <Link className="text-cyan-100" to="projects">
+        <Link
+          className={
+            location.pathname === "/projects"
+              ? "text-green-400 text-lg"
+              : "text-cyan-100"
+          }
+          to="projects"
+        >
           Projects
         </Link>
-        <Link className="text-cyan-100" to="contact">
+        <Link
+          className={
+            location.pathname === "/contact"
+              ? "text-green-400 text-lg"
+              : "text-cyan-100"
+          }
+          to="contact"
+        >
           Contact
         </Link>
-        <button className="text-cyan-100">Resume</button>
+        <button className="text-cyan-100 border-solid border-2 border-green-400 p-2 rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100">
+          Resume
+        </button>
       </ul>
     </nav>
   );
