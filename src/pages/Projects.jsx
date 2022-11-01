bc513d29761e3fb7566be8b13c81a383c0920125
Ben Smerd <bsmerd@adam.com.au>
2022-11-01 14:09:49 +1030

    create projects container

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,8 @@
 const Projects = () => {
   return (
-    <section id="projects" className=" bg-gray-900 w-full h-screen">
-      <div className="flex flex-col gap-3 font-mono w-3/6 h-screen p-20 ml-40">
-        <h1 className="text-green-200">Projects page</h1>
+    <section id="projects" className=" bg-gray-900 w-full h-screen pt-24">
+      <div className="border-white border-solid border-2 flex flex-col gap-3 font-mono w-4/6 h-[34rem] ml-20">
+        <h1 className="text-4xl text-green-400 font-bold">Projects page</h1>
       </div>
     </section>
   );
