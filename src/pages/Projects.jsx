390a54e75ad9927c34edd2783161e9ea470a22bc
Ben Smerd <bsmerd@adam.com.au>
2022-11-01 14:25:25 +1030

    added in images of projects and imported in projects file

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,28 @@
+import KryptoHack from "../images/KryptoHack.png";
+
 const Projects = () => {
   return (
     <section id="projects" className=" bg-gray-900 w-full h-screen pt-24">
       <div className="border-white border-solid border-2 flex flex-col gap-3 font-mono w-4/6 h-[34rem] ml-20">
         <h1 className="text-4xl text-green-400 font-bold">Projects page</h1>
+        <ul className="flex flex-wrap gap-8">
+          <li>
+            <img src={KryptoHack} alt={`${KryptoHack}`} className="h-52" />
+            <p className="text-green-500">CoinCharter</p>
+          </li>
+          <li>
+            <img src={KryptoHack} alt={`${KryptoHack}`} className="h-52" />
+            <p className="text-green-500">CoinCharter</p>
+          </li>
+          <li>
+            <img src={KryptoHack} alt={`${KryptoHack}`} className="h-52" />
+            <p className="text-green-500">CoinCharter</p>
+          </li>
+          <li>
+            <img src={KryptoHack} alt={`${KryptoHack}`} className="h-52" />
+            <p className="text-green-500">CoinCharter</p>
+          </li>
+        </ul>
       </div>
     </section>
   );
