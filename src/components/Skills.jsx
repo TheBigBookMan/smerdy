a548b6f542c43192d4451a50e4e3bc7cfa3f020e
Ben Smerd <bsmerd@adam.com.au>
2022-11-02 14:07:10 +1030

    added skills list animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,15 @@ const Skills = () => {
   return (
     <ul className="flex mx-auto gap-[50px]">
       {skills.map((skill) => (
-        <li className=" flex flex-col gap-1">
-          <h3 className="text-zinc-400 text-xl">{skill.skillTitle}</h3>
-          <ul className="text-zinc-50 flex flex-col gap-2">
+        <li className="group relative flex flex-col gap-1">
+          <h3 className="relative text-zinc-400 text-xl transition-all cursor-pointer group-hover:text-green-400">
+            {skill.skillTitle}
+          </h3>
+          <ul className="text-zinc-50 flex flex-col gap-2 duration-1000">
             {skill.skills.map((skillArr) => (
-              <li>{skillArr}</li>
+              <li className=" top-0 left-0 h-0 opacity-0 group-hover:h-full group-hover:opacity-100 duration-1000 ">
+                {skillArr}
+              </li>
             ))}
           </ul>
         </li>
