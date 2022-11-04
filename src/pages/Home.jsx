fdecea897b5cf724e6973c06fbed36dbc5c2b779
Ben Smerd <bsmerd@adam.com.au>
2022-11-05 10:25:30 +1030

    added in icons and links to personal social pages on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,37 @@ const Home = () => {
           I like to build dynamic and creative front-end applications while
           integrating logical and smooth running back-end databases and APIs.
         </h3>
-        <Link
-          className="w-60 mt-3 text-cyan-100 border-solid border-2 border-green-400 p-3 text-center rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100"
-          smooth
-          to="#projects"
-        >
-          Check out my Projects!
-        </Link>
+        <div className="flex gap-12 items-center  mt-3">
+          <Link
+            className="w-60 text-cyan-100 border-solid border-2 border-green-400 p-3 text-center rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100 transition-all"
+            smooth
+            to="#projects"
+          >
+            Check out my Projects!
+          </Link>
+          <ul className="flex gap-12 text-5xl text-green-500">
+            <li>
+              <a href="https://github.com/TheBigBookMan" target="_blank">
+                <i className="fa-brands fa-github hover:text-cyan-100"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/bensmerd/" target="_blank">
+                <i className="fa-brands fa-linkedin hover:text-cyan-100"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/smerdy/" target="_blank">
+                <i className="fa-brands fa-instagram hover:text-cyan-100"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/BenSmerd" target="_blank">
+                <i className="fa-brands fa-twitter hover:text-cyan-100"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
