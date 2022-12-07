db631f37d10bb40dc29f6503a3016d924b6326ed
Ben Smerd <bsmerd@adam.com.au>
2022-12-07 19:05:23 +1030

    added in emailjs for users to contact me in contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import React, { useRef } from "react";
 import { HashLink as Link } from "react-router-hash-link";
+import emailjs from "@emailjs/browser";
+
+//TODO add in cheeky toaster pop up for when message sent
 
 const Contact = () => {
-  const [formDetails, setFormDetails] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const changeDetails = (e) => {
-    setFormDetails({
-      ...formDetails,
-      [e.target.name]: e.target.value,
-    });
-  };
+  const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    console.log(formDetails);
+
+    emailjs
+      .sendForm(
+        "service_ouwljep",
+        "template_rabp8o9",
+        form.current,
+        "RQYS-44VuF0Vo86Ns"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("Message sent!");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   return (
@@ -28,43 +36,36 @@ const Contact = () => {
         </h1>
         <p className="text-zinc-50">
           My preferred method of contact is through Email:{" "}
-          <span className="text-green-500">bsmerd@adam.com.au</span> or feel
-          free to send a message on my socials listed{" "}
+          <span className="text-green-500">bjsmerd@gmail.com</span> or feel free
+          to send a message on my socials listed{" "}
           <Link className="text-green-500 hover:text-zinc-400" to="/#" smooth>
             above.
           </Link>
         </p>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" ref={form} onSubmit={sendEmail}>
           <input
             type="text"
-            name="name"
-            onChange={changeDetails}
-            value={formDetails.name}
+            name="user_name"
             placeholder="* preferred name..."
             className="rounded-lg pl-1 p-1 border-green-400 border-solid border-4 outline-0"
           />
           <input
-            type="text"
-            name="email"
-            onChange={changeDetails}
-            value={formDetails.email}
+            type="email"
+            name="user_email"
             placeholder="* email address..."
             className="rounded-lg pl-1 p-1 border-green-400 border-solid border-4 outline-0"
           />
           <textarea
             name="message"
-            onChange={changeDetails}
-            value={formDetails.message}
             placeholder="* send a message..."
             rows={4}
             className="rounded-lg pl-1 p-1 border-green-400 border-solid border-4 outline-0"
           />
-          <button
-            onClick={sendEmail}
-            className="w-60 text-cyan-100 border-solid border-2 border-green-400 p-3 text-center rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100 transition-all"
-          >
-            Send Message
-          </button>
+          <input
+            type="submit"
+            value="Send"
+            className="hover:cursor-pointer w-60 text-cyan-100 border-solid border-2 border-green-400 p-3 text-center rounded-2xl hover:bg-green-400 active:text-green-400 active:bg-cyan-100 transition-all"
+          />
         </form>
       </div>
     </section>
