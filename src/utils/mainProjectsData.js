45e4359d19af5b22b4239f4bc8dd3aa64e9b3e06
Ben Smerd <bsmerd@adam.com.au>
2022-12-08 17:39:57 +1030

    updated main project and side project information

--- src/utils/mainProjectsData.js
+++ src/utils/mainProjectsData.js
@@ -13,8 +13,10 @@ export const mainProjectData = [
     github: "https://github.com/bradbrad88/coin-charter",
     description:
       "This is a social crypto coin app that uses the MERN stack and GraphQL with a coingeko crypto third party API. I worked collaboratively in a team and mainly worked on front-end components, the database, routes and logic.",
-    mainDescription: "",
-    technologies: "",
+    mainDescription:
+      "!!The URL is currently down as we are migrating the database!!- This application is a social media/crypto analysis/technical analysis combination of concepts. Users can create an account, add friends, make comments, view friends activity and connect with friends. The user can also view cryptocurrencies and their information, while being able to save them to favourites, make comments on the coin and view others comments. Users can also create technical analysis for charts of the coins and upload a screenshot for others to view, comment and upvote. We are currently migrating the database and ORM from MongoDB/Mongoose to PostgreSQL/Prisma and (it was in MongoDB/Mongoose for our bootcamp required criteria) and we are searching to get a proper trading chart feature where users can save the actual chart to the database so other users can interact with it, rather than just a screenshot.",
+    technologies:
+      "Typescript/Javascript, ReactJS, NodeJS, JWT, TailwindCSS, GraphQL/Apollo, MongoDB/Mongoose, Third Party APIs, AWS S3 Buckets and multiple NPM packages.",
   },
   {
     title: "Employee Tracker",
@@ -23,9 +25,10 @@ export const mainProjectData = [
       "https://drive.google.com/file/d/15rB57lI7IGguM8PixYzD2ICHcf95mXYn/view",
     github: "https://github.com/TheBigBookMan/Employee-Tracker",
     description:
-      "Backend application I created with NodeJS, ExpressJS and MySql. A server-side app that allows the user to view information about a workplace through querying the database. They can query about departments, roles and employees information.",
-    mainDescription: "",
-    technologies: "",
+      "A server-side app that allows the user to view information about a workplace through querying the database. They can query about departments, roles and employees information.",
+    mainDescription:
+      "I created a relational database with REST API for the user to be able to perform complete CRUD queries and view the database within the command line. The user will be asked questions about what they would like to within the application by using the NPM package of Inquirer. The user will be able to view the database in the form of a table using the console.table NPM package, increasing a user experience.",
+    technologies: "NodeJS, ExpressJS, MySql and NPM packages.",
   },
   {
     title: "Empty Your Fridge",
@@ -34,8 +37,10 @@ export const mainProjectData = [
     github: "https://github.com/TheBigBookMan/Empty-Your-Fridge",
     description:
       "This is a ReactJS client-side application where users can input ingredients that they have laying around at home and select the type of meal they want and then multiple recipes will be presented for them to choose for a meal to empty their fridge!",
-    mainDescription: "",
-    technologies: "",
+    mainDescription:
+      "I have been a student for as long as I could remember and this meant that I would always have some random ingredients lying around with no idea what to cook with them. I created this mobile-first application with the idea that users will have random ingredients lying around and they can input them into the application (with the select criteria) and then be able to view multiple recipes. They can then click on the recipes to find what other ingredients they will need to buy and how to make the food. I think of it as reverse engineering recipe-hunting. It is handy that the users can then save recipes to favourites for later if they would like to. I might further develop it by making it a PWA and allowing users to view their favourited recipes while offline.",
+    technologies:
+      "ReactJS, Javascript, TailwindCSS, Local Storage, Third Party API, React Packages and Responsive Design.",
   },
   {
     title: "EntertainNow",
@@ -44,7 +49,9 @@ export const mainProjectData = [
     github: "https://github.com/TheBigBookMan/EntertainNow",
     description:
       "Full-stack, mobile first entertainment application where users can input entertainment criteria and then view the youtube trailer for what they are interested in. There is also user authentication and using a database to add to favourites.",
-    mainDescription: "",
-    technologies: "",
+    mainDescription:
+      "!!Currently having deployment issues!!- I built this full-stack application with the intention of learning more about Typescript, GraphQL and JWT user authorization. This was a really good test of a project for me as the scale of it wasn't big but the niche parts of it were a good learning step. I had always found that most entertainment search websites would allow you to browse around but never actually view the trailer for what you wanted, so I created this to be an easier method of browsing and viewing trailers. The user can also create an account and add entertainment to favourites to watch the trailers again later on if they want. I had trouble with adding in cookies to local storage/JWT, but I will work on that in future development for extra security.",
+    technologies:
+      "ReactJS, ExpressJS, NodeJS, Typescript/Javascript, TailwindCSS, Vite, GraphQL/Apollo, MongoDB/Mongoose, Third Party API, JWT, React Packages, and responsive design.",
   },
 ];
--- src/utils/sideProjectsData.js
+++ src/utils/sideProjectsData.js
@@ -4,25 +4,23 @@ export const sideProjectData = [
     img: "",
     deployed: "",
     github: "https://github.com/TheBigBookMan/smerdy",
-    mainDescription: "",
-    technologies: "",
-  },
-  {
-    title: "ReadME Generator",
-    img: "",
-    deployed:
-      "https://drive.google.com/file/d/1EuQuHXA-1whJwjeJi644p5tQqawym99Z/view",
-    github: "https://github.com/TheBigBookMan/ReadME-Generator",
-    mainDescription: "",
-    technologies: "",
+    description: "",
+    mainDescription:
+      "I wanted to create a portfolio that stood out a little bit more than the rest. In my bootcamp we had to make portfolios, so I was lucky enough to have the chance to view other people in classes. But they really all felt kind of the same, following a specific template we were given. I decided to branch away and do my own sort of thing. I really enjoy learning new Javascript and React technologies, so creating a portfolio that shows off my curiosity I figured would be a smart decision. That's why I have tried to implement some more graphic design/3d modeling with ThreeJS.",
+    technologies:
+      "ReactJS, Javascript, TailwindCSS, ThreeJS, React Packages and responsive design.",
   },
   {
     title: "Houride",
     img: "",
     deployed: "",
     github: "https://github.com/TheBigBookMan/Houride",
-    mainDescription: "",
-    technologies: "",
+    description:
+      "A car rental application where users can rent out their car for specific events and customers can pay per hour for the service.",
+    mainDescription:
+      "!!I am currently building this and it will take a while because it's very large application!!- My friend and I are trying to start a business where users will be able to sign up their car and drive people to events while being paid per hour. This would be for people who are looking for fancier cars to be taken to formals, weddings etc or wanting a bigger vehicle for a larger crowded event. This application is currently in development and is a great learning step for myself by using popular technologies- such as Typescript, React, GraphQL and PostgreSQL. I am enjoying the experience so far and will be working on this for a few months while I continue to work on smaller solo projects.",
+    technologies:
+      "ReactJS, Typescript/Javascript, TailwindCSS, Mobile-First Responsive Design, Vite, React Packages, GraphQL/Apollo, ExpressJS, NodeJS, NPM Packages, PostgreSQL, Prisma, JWT.",
   },
   {
     title: "Team Profile Generator",
@@ -30,23 +28,57 @@ export const sideProjectData = [
     deployed:
       "https://drive.google.com/file/d/1OURcazg1_q24l2h6Id1PUNoIRHLmJ9tE/view",
     github: "https://github.com/TheBigBookMan/Team-Profile-Generator",
-    mainDescription: "",
-    technologies: "",
+    description:
+      "This project utilizes the back-end programming language NodeJS with OOP, as well as front end languages- HTML, CSS and Javascript. The user will be prompted with inputting information about their employees and the program will create a HTML and CSS file with the information. The user will be able to interact with information such as email and github accounts for easy access to contacting the employees.",
+    mainDescription:
+      "This application was used with Object-Oriented Programming to design the layout of information for employees. Using the inquirer package, the user will be prompted with questions in relation to their employees. The user will input information and then a profile manager application will launch with the information added in.",
+    technologies: "JEST, CSS, Javascript, OOP, NodeJS, NPM Packages.",
   },
+  {
+    title: "WhatToRead",
+    img: "",
+    deployed: "",
+    github: "https://github.com/TheBigBookMan/WhatToRead",
+    description:
+      "This is a server-side book search database for a library, where users will be able to perform CRUD operations to query the database for information about certain books and leave reviews for others.",
+    mainDescription:
+      "!!Currently in development!!- I am wanting to increase my server-side logic and command line skills, so I thought it would be great to create a library book database. Users will be able to query the database through the Inquirer package and find information on interested books. The users will also be able to see if the books are in stock, leave reviews for others and put them on hold. This app will also use JWT for the user to be authenticated when logging in. The use of a relational database will be needed due to the relationships of books, customers and review comments.",
+    technologies:
+      "NodeJS, ExpressJS, GraphQL, PostgreSQL, Prisma, Third Party API, NPM Packages, JWT",
+  },
+  {
+    title: "ReadME Generator",
+    img: "",
+    deployed:
+      "https://drive.google.com/file/d/1EuQuHXA-1whJwjeJi644p5tQqawym99Z/view",
+    github: "https://github.com/TheBigBookMan/ReadME-Generator",
+    description:
+      "Users are able to input information they want on their ReadME file and then the ReadME file will be created into their root directory.",
+    mainDescription:
+      "Once the application has started, the user will be prompter with some questions in regards to the README file content. The questions will require answers in the form of text input, list selection or no response (with default choice). These prompts are created by importing the dependency Inquirer in the command line. A README file will be generated within their root folder.",
+    technologies: "NodeJS, NPM Package- Inquirer",
+  },
+
   {
     title: "Weather App",
     img: "",
     deployed: "https://thebigbookman.github.io/Weather-Dashboard/",
     github: "https://github.com/TheBigBookMan/Weather-Dashboard",
-    mainDescription: "",
-    technologies: "",
+    description:
+      "This is a client-side weather app where users can search for a city they want to know the weather for. The weather will show up for current day and for 5 days afterwards. There is a history of each city that the user searches so they can view later.",
+    mainDescription:
+      "This was one of the first applications I made after starting to learn to code. I was about 4 weeks into my coding bootcamp and had just started learning Javascript and APIs. This is a basic weather app that utilizes API calling and logic to determine the correct information is being presented on the screen. Using the local storage to store the history was a good learning step as to what computers can do and how programmers can interact locally with every user.",
+    technologies: "HTML, CSS, Javascript, Third Party API and Local Storage",
   },
   {
-    title: "Cafe Template",
+    title: "Coffee & Bagels",
     img: "",
     deployed: "https://thebigbookman.github.io/Cafe-template/",
     github: "https://github.com/TheBigBookMan/Cafe-template",
-    mainDescription: "",
-    technologies: "",
+    description:
+      "This is a static website template for a cafe, very basic design which allows the cafe to show off their food and drinks menu.",
+    mainDescription:
+      "I created this in my spare time while halfway through the bootcamp as I wanted to test out my CSS skills with someone simple. I like the idea of being able to build real world applications for businesses, and in te future I may further develop this application so the business can link up their Instagram and Facebook so they have a live feed of their posts.",
+    technologies: "HTML and CSS",
   },
 ];
