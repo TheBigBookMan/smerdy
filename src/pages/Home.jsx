34c40e33d2ed074b15783214e3d68dd866fd367a
Ben Smerd <bsmerd@adam.com.au>
2022-12-12 16:11:38 +1030

    added in 3d text of my name using react three fiber drei

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,10 @@ import {
   AiFillInstagram,
   AiOutlineTwitter,
 } from "react-icons/ai";
+import ThreeBall from "../components/ThreeBall";
+import { Text3D, Center } from "@react-three/drei";
+import Roboto_Bold from "../files/Roboto_Bold.json";
+import { Canvas } from "@react-three/fiber";
 
 // TODO ADD IN A THREEJS ANIMATED CUBE TO THE RIGHT OR SOMETHING ANIMATED TO LOOK COOL
 //TODO COULD ADD IN THOSE WAVEY DOTS AND THEN HAVE 3D OF MY NAME THAT CAN MOVE AROUND ON THE HOME PAGE, WOULD BE REALLY COOL
@@ -18,13 +22,26 @@ import {
 //TODO need to have the url change when scrolling--- will have to search how to do that
 
 const Home = () => {
+  const TextThree = () => {
+    return (
+      <Canvas>
+        <Center center>
+          <Text3D font={Roboto_Bold} size={5} letterSpacing={1.25}>
+            Ben Smerd
+            <meshNormalMaterial />
+          </Text3D>
+        </Center>
+      </Canvas>
+    );
+  };
+
   return (
     <section id="#" className="  bg-gray-900 w-full h-full snap-center">
       <div className="z-20 relative flex flex-col gap-3 font-mono h-full w-full pt-24 p-8 max-w-4/6 md:p-20 md:ml-40 md:pt-40">
         <small className="text-green-400">Hi, my name is</small>
-        <h1 className="text-2xl md:text-8xl text-zinc-50 font-bold">
-          Ben Smerd.
-        </h1>
+        <div className=" h-[40px] md:h-[80px]">
+          <TextThree />
+        </div>
         <h2 className="text-green-500 text-2xl md:text-5xl">
           I am a full-stack web developer.
         </h2>
@@ -81,6 +98,7 @@ const Home = () => {
             </li>
           </ul>
         </div>
+        <ThreeBall />
       </div>
     </section>
   );
