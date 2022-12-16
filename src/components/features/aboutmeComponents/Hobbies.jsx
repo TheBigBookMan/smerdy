47201f0ed30101fb52abf8e86929c439b4487724
Ben Smerd <bsmerd@adam.com.au>
2022-12-16 18:12:09 +1030

    added react icons to the hobbies list

--- src/components/features/aboutmeComponents/Hobbies.jsx
+++ src/components/features/aboutmeComponents/Hobbies.jsx
@@ -3,7 +3,17 @@ import Drifting from "../../../images/hobbiesImages/guitar/Drifting.png";
 import Big from "../../../images/hobbiesImages/hiking/Big.jpg";
 import Surfing from "../../../images/hobbiesImages/sport/Surfing.png";
 import Turkey from "../../../images/hobbiesImages/travel/Turkey.png";
+import { Link } from "react-router-dom";
+import { usePageContext } from "../../context/PageContext";
+import {
+  FaGuitar,
+  FaHiking,
+  FaMapMarkerAlt,
+  FaRunning,
+  FaRobot,
+} from "react-icons/fa";
 
+const reactIcons = [FaRobot, FaGuitar, FaHiking, FaMapMarkerAlt, FaRunning];
 const thumbnails = [
   {
     image: DalorianBeach,
@@ -27,21 +37,37 @@ const thumbnails = [
   },
 ];
 
+//!
+//! DO RESPONSIVENESS
+//!
+
 const Hobbies = () => {
+  const { setIsHomePage } = usePageContext();
+
   return (
-    <ul className="flex flex-col items-center gap-2 h-[360px] overflow-y-scroll scrollbar-thin scrollbar-thumb-green-400 scrollbar-track-zinc-50 scrollbar-thumb-rounded scrollbar-track-rounded">
-      {thumbnails.map((hobby) => (
-        <li className="relative group hover:cursor-pointer">
-          <img
-            className="relative w-[280px] h-[260px] rounded-3xl group-hover:brightness-50 transition-all"
-            alt={hobby.category}
-            src={hobby.image}
-          />
-          <h1 className="transition-all opacity-0 group-hover:opacity-100 font-bold text-green-400 text-2xl absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            {hobby.category}
-          </h1>
-        </li>
-      ))}
+    <ul className="flex flex-col items-center gap-2 h-[400px] overflow-y-scroll scrollbar-thin scrollbar-thumb-green-400 scrollbar-track-zinc-50 scrollbar-thumb-rounded scrollbar-track-rounded">
+      {thumbnails.map((hobby, idx) => {
+        const Icon = reactIcons[idx];
+        return (
+          <Link
+            onClick={() => setIsHomePage(false)}
+            to={`/hobbie/${hobby.category}`}
+            className="relative group hover:cursor-pointer"
+          >
+            <img
+              className="relative w-[280px] h-[260px] rounded-3xl group-hover:brightness-50 transition-all"
+              alt={hobby.category}
+              src={hobby.image}
+            />
+            <div className="flex gap-2 items-center transition-all opacity-0 group-hover:opacity-100 absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <h1 className="font-bold text-green-400 text-2xl">
+                {hobby.category}
+              </h1>
+              <Icon className="text-cyan-500 text-3xl" />
+            </div>
+          </Link>
+        );
+      })}
     </ul>
   );
 };
